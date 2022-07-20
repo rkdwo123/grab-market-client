969cac34b571704f8f543f2937e6065e2931efa5
unknown <rkdwo123@hanmail.net>
2022-07-20 15:15:57 +0900

    first commit

---
+++ src/child.js
@@ -0,0 +1,13 @@
+function ChildComponent(props) {
+  const { name, age } = props;
+
+  return (
+    <div>
+      <p>
+        이름은 {name} 입니다. 나이는 {age} 입니다.
+      </p>
+    </div>
+  );
+}
+
+export default ChildComponent;
